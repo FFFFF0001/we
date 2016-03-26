47e81caf90f9be8c4acf254d3223c2b879d71ccd
mj <13211239457@163.com>
2016-03-26 18:37:39 +0800

    更新验证模块和新加个人主页设置

--- static/CXCL/js/num.js
+++ static/CXCL/js/num.js
@@ -8,11 +8,15 @@ define(['jquery','bootstrap'],function(){
         function num(object,min,max){    //object jq对象
             if(object.val().length<min || object.val().length>max) {
                 var text="请输入"+min+"-"+max+"个字符串";
-                object.popover({placement:"top",title:text});
+                object.parent().addClass("has-error");
+                object.popover({placement:"top",content: '<div id="content" style="color:#a94442">'+text+'</div>',trigger:"manual",html: true});
                 object.popover('show');
+                return false;
             }
             else{
+                object.parent().removeClass("has-error");
                 object.popover('hide');
+                return true;
             }
         }
         return num;
--- static/CXCL/js/pwd_auth.js
+++ static/CXCL/js/pwd_auth.js
@@ -6,12 +6,17 @@
 * */
 define(['jquery','bootstrap'],function(){
         function pwd_auth(pwd1,pwd2){    //pwd1 第一次输入,pwd2 第二次输入
-            if(pwd1.val().length!=pwd2.val().length) {
-                pwd2.popover({placement:"top",title:"两次密码输入不一致"});
+            if($.trim(pwd1.val())!=$.trim(pwd2.val()))
+            {
+                pwd2.parent().addClass("has-error");
+                pwd2.popover({placement:"top",content: '<div id="content" style="color:#a94442">两次密码不一致</div>',trigger:"manual",html: true});
                 pwd2.popover('show');
+                return false;
             }
             else{
+                pwd2.parent().removeClass("has-error");
                 pwd2.popover('hide');
+                return true;
             }
         }
         return pwd_auth;
